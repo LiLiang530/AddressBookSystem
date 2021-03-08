2225e62b536670c58ea7f7c019a4aa81ac3dbcb9
LiLiang530 <80199330+LiLiang530@users.noreply.github.com>
2021-03-08 19:31:42 +0800

    Update ShowInfoUI.java

--- src/ShowInfoUI.java
+++ src/ShowInfoUI.java
@@ -107,7 +107,7 @@ public class ShowInfoUI extends JFrame{
             }
             try {
                 con = DriverManager.getConnection
-                        ("jdbc:sqlserver://localhost:1433;DatabaseName=address", "sa", "2ooo0530");
+                        ("jdbc:sqlserver://localhost:1433;DatabaseName=address", "sa", "xxxxxxxx");
                 System.out.println("数据库连接成功");
             } catch (Exception exy) {
                 System.out.println("数据库连接失败");
@@ -146,7 +146,7 @@ public class ShowInfoUI extends JFrame{
             }
             try {
                 con = DriverManager.getConnection
-                        ("jdbc:sqlserver://localhost:1433;DatabaseName=address", "sa", "2ooo0530");
+                        ("jdbc:sqlserver://localhost:1433;DatabaseName=address", "sa", "xxxxxxxx");
                 System.out.println("数据库连接成功");
             } catch (Exception exy) {
                 System.out.println("数据库连接失败");
