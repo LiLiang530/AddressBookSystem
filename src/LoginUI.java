2cf73778f4d058ccde5143afab691ddf0572954f
LiLiang530 <80199330+LiLiang530@users.noreply.github.com>
2021-03-08 19:00:22 +0800

    Update LoginUI.java

--- src/LoginUI.java
+++ src/LoginUI.java
@@ -73,7 +73,7 @@ public class LoginUI extends JFrame implements ActionListener{
             }
             try {
                 con = DriverManager.getConnection
-                        ("jdbc:sqlserver://localhost:1433;DatabaseName=address","sa","2ooo0530");
+                        ("jdbc:sqlserver://localhost:1433;DatabaseName=address","sa","xxxxxxxx");
                 System.out.println("数据库连接成功");
             } catch (Exception exy) {
                 System.out.println("数据库连接失败");
